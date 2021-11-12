02596e1f2030fbb05b93865ccefeb4c7e345926a
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-11-12 22:17:54 +0900

    Adjust profile css

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,16 @@ export default function Home() {
       <Map />
       <ActionItems>
         {/* {Headers} */}
+        <Header>
+          <UberLogo src="https://pbs.twimg.com/profile_images/1431005685307691009/htnq498e_400x400.jpg"></UberLogo>
+          <Profile>
+            <Name>Nanashi</Name>
+            <UserImage
+              src="https://pbs.twimg.com/profile_images/1431005685307691009/htnq498e_400x400.jpg"
+              alt="Nanashi"
+            />
+          </Profile>
+        </Header>
         {/* {ActionButtons} */}
         {/* {InputButton} */}
       </ActionItems>
@@ -19,11 +29,39 @@ export default function Home() {
 
 const Wrapper = tw.div`
   flex flex-col
-  bg-gray-300
   h-screen
 `;
 
 const ActionItems = tw.div`
-  bg-red-200
   flex-1
+  p-4
 `;
+
+const Header = tw.div`
+  flex flex-row
+  justify-between
+  items-center
+  `;
+
+const UberLogo = tw.img`
+  h-28
+  w-28`;
+
+const Profile = tw.div`
+  flex
+  items-center
+  `;
+
+const Name = tw.div`
+  mr-3 
+  w-20
+  text-sm
+`;
+
+const UserImage = tw.img`
+  h-12
+  w-12
+  rounded-full
+  border-gray-400
+  p-px
+  `;
