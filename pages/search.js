e99b823189da9e5fca3a780c1c1baa8b711029c9
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-11-20 20:37:16 +0900

    Add a new page for search

---
+++ pages/search.js
@@ -0,0 +1,17 @@
+import React from "react";
+import tw from "tailwind-styled-components";
+
+const search = () => {
+  return (
+    <Wrapper>
+      {/* Button Container */}
+      {/* Input container */}
+      {/* Saved Places */}
+      {/* Confirm Location */}
+    </Wrapper>
+  );
+};
+
+export default search;
+
+const Wrapper = tw.div``;
