3021aacf7b953497c172514942da0e5b4b9a2748
Randy, Huang <s104021615@gapp.nthu.edu.tw>
2021-11-20 20:36:50 +0900

    Add Action Buttons

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import tw from "tailwind-styled-components";
 import Map from "./components/Map";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,13 +14,31 @@ export default function Home() {
         <Header>
           <UberLogo src="https://pbs.twimg.com/profile_images/1431005685307691009/htnq498e_400x400.jpg"></UberLogo>
           <Profile>
-            <Name>Nanashi</Name>
+            <Name>Nanashi Mumei</Name>
             <UserImage
               src="https://pbs.twimg.com/profile_images/1431005685307691009/htnq498e_400x400.jpg"
               alt="Nanashi"
             />
           </Profile>
         </Header>
+        <ActionButtons>
+          <Link href="/search">
+            <ActionButton>
+              <ActionButtonImage src="https://pbs.twimg.com/media/FDEwJ3JUYAgmy8N?format=jpg" />
+              Ride
+            </ActionButton>
+          </Link>
+          <ActionButton>
+            <ActionButtonImage src="https://pbs.twimg.com/media/FDEwJ3JUYAgmy8N?format=jpg" />
+            Wheels
+          </ActionButton>
+          <ActionButton>
+            <ActionButtonImage src="https://pbs.twimg.com/media/FDEwJ3JUYAgmy8N?format=jpg" />
+            Reserver
+          </ActionButton>
+        </ActionButtons>
+
+        <InputButton>Where to go?</InputButton>
         {/* {ActionButtons} */}
         {/* {InputButton} */}
       </ActionItems>
@@ -28,7 +47,8 @@ export default function Home() {
 }
 
 const Wrapper = tw.div`
-  flex flex-col
+  flex 
+  flex-col
   h-screen
 `;
 
@@ -38,7 +58,8 @@ const ActionItems = tw.div`
 `;
 
 const Header = tw.div`
-  flex flex-row
+  flex
+  flex-row
   justify-between
   items-center
   `;
@@ -64,4 +85,39 @@ const UserImage = tw.img`
   rounded-full
   border-gray-400
   p-px
+  `;
+
+const ActionButtons = tw.div`
+  flex
+`;
+
+const ActionButton = tw.button`
+  flex
+  bg-gray-200
+  flex-1
+  m-1
+  h-28
+  items-center
+  flex-col
+  justify-center
+  rounded-lg
+  transform
+  hover:bg-gray-300
+  hover:scale-105
+  transition
+`;
+
+const ActionButtonImage = tw.img`
+  h-3/5
+`;
+
+const InputButton = tw.button`
+  h-20
+  w-full
+  bg-gray-200
+  text-2xl
+  p-4
+  flex
+  items-center
+  mt-4
   `;
